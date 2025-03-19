c9054552db4f83eaa53a4a4205c2fa9e2c6f7118
Ravshan Samandarov <ravshansbox@gmail.com>
2025-03-19 20:44:18 +0300

    create migrations folder if it does not exist

--- src/bin/migrate.ts
+++ src/bin/migrate.ts
@@ -5,17 +5,25 @@ import { migrateDown, migrateSync, migrateUp } from '../index.js';
 import { logger } from '../logger.js';
 import { template } from '../constants.js';
 
+const dir = path.join(process.cwd(), 'migrations');
+
 (async () => {
   const command = process.argv[2];
   if (command === 'make') {
-    const name = process.argv[3];
-    if (!name) {
+    const migrationName = process.argv[3];
+    if (!migrationName) {
       logger.error('Provide a migration name\n');
       process.exit(1);
     }
-    const filename = path.join(process.cwd(), `migrations/${name}.sql`);
-    await fs.writeFile(filename, template);
-    logger.info(`Migration ${filename} created`);
+    try {
+      await fs.stat(dir);
+    } catch (_error) {
+      await fs.mkdir(dir, { recursive: true });
+      logger.info(`Created migrations directory: ${dir}`);
+    }
+    const filename = `${migrationName}.sql`;
+    await fs.writeFile(path.join(dir, filename), template);
+    logger.info(`Migration ${path.join(dir, filename)} created`);
   } else if (command === 'up') {
     await migrateUp();
   } else if (command === 'down') {
--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,7 @@
+import { EOL } from 'node:os';
+
 export const {
   DATABASE_URL = 'postgres://postgres:postgres@localhost:5432/postgres',
 } = process.env;
 
-export const template = `
--- Up Migration
---
--- Down Migration
---
-`.trim();
+export const template = `-- Up Migration${EOL}-- ${EOL}-- Down Migration${EOL}-- ${EOL}`;
