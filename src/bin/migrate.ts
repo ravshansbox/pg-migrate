98e9c913ec6293ec94de2b7036af4324ec31d2ee
Ravshan Samandarov <ravshansbox@gmail.com>
2025-03-19 15:10:59 +0300

    implement `make` command

--- src/bin/migrate.ts
+++ src/bin/migrate.ts
@@ -1,11 +1,30 @@
 #!/usr/bin/env node
+import fs from 'node:fs/promises';
+import path from 'node:path';
 import { migrateDown, migrateSync, migrateUp } from '../index.js';
 import { logger } from '../logger.js';
+
+const template = `
+-- Up Migration
+--
+-- Down Migration
+--
+`.trim();
 
 const command = process.argv[2];
 
 (async () => {
-  if (command === 'up') {
+  if (command === 'make') {
+    const name = process.argv[3];
+    if (!name) {
+      logger.error('Provide a migration name\n');
+      process.exit(1);
+    }
+    await fs.writeFile(
+      path.join(process.cwd(), `migrations/${name}.sql`),
+      template,
+    );
+  } else if (command === 'up') {
     await migrateUp();
   } else if (command === 'down') {
     await migrateDown();
